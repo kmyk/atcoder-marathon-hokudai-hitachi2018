bd25c10debf6a46ecb77184e072028d4590c99b9
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-02-15 00:46:04 +0900

    refactoring

--- main.cpp
+++ main.cpp
@@ -61,23 +61,13 @@ term_t make_term(int c, vector<int> const & v) {
     return (term_t) { c, v };
 }
 
-int get_max_c(vector<term_t> const & f) {
+int get_maxcoeff(vector<term_t> const & f) {
     int max_c = 0;
     for (auto const & t : f) {
         if (t.v.empty()) continue;  // ignore constants
         chmax(max_c, t.c);
     }
     return max_c;
-}
-
-int get_constant_term(vector<term_t> const & f) {
-    int sum_c = 0;
-    for (auto const & t : f) {
-        if (t.v.empty()) {
-            sum_c += t.c;
-        }
-    }
-    return sum_c;
 }
 
 int apply_all_true(vector<term_t> const & f) {
@@ -132,7 +122,7 @@ int apply_vector_min_sa(int m, vector<term_t> const & g, vector<bool> const & x,
     int value = apply_argumented_vector(g, x, w);
     int min_value = value;
 
-    int total = 10 * m;
+    int total = max(100, 10 * m);
     REP (iteration, total) {
         double temperature = (double) (total - iteration) / total;
 
@@ -158,50 +148,6 @@ template <class Generator>
 int apply_all_true_min_sa(int n, int m, vector<term_t> const & g, Generator & gen) {
     vector<bool> x(n, true);
     return apply_vector_min_sa(m, g, x, gen);
-}
-
-int apply_vector_min(vector<term_t> const & g, vector<bool> const & x, int m) {
-    int n = g.size() - m;
-    int min_value = INT_MAX;
-    REP (w, 1 << m) {
-        int value = 0;
-        for (auto const & t : g) {
-            for (int i : t.v) {
-                if (not (i < n ? x[i] : w & (1 << (i - n)))) {
-                    goto next;
-                }
-            }
-            value += t.c;
-    next: ;
-        }
-        chmin(min_value, value);
-    }
-    return min_value;
-}
-
-vector<term_t> extract_low_degree_terms(vector<term_t> const & f) {
-    vector<term_t> g;
-    for (auto const & t : f) {
-        if (t.v.size() <= 2) {
-            g.push_back(t);
-        }
-    }
-    return g;
-}
-
-vector<term_t> extract_initial_polynomial(vector<term_t> const & f) {
-    vector<term_t> g;
-    for (auto const & t : f) {
-        if (t.v.size() <= 2) {
-            g.push_back(t);
-        } else {
-            int d = t.v.size();
-            REP (j, d) REP (i, j) {
-                g.push_back(make_term(t.c / (d * (d - 1) / 2), { i, j }));
-            }
-        }
-    }
-    return g;
 }
 
 pair<int, vector<term_t> > remove_unused_newvars(int n, int m, vector<term_t> g) {
@@ -273,35 +219,46 @@ vector<term_t> merge_terms(int l, vector<term_t> const & g) {
     return h;
 }
 
-double evaluate_relaxed_score1(int m, int l, int max_c, int delta) {
-    // constexpr double a = 10000;
-    constexpr double b = 5;
-    // constexpr double e = 10000;
-    constexpr double t = 100;
-    double px = delta >= 0 ? (1 - min<double>(t, delta) / t) : 0;
-    double px1 = (1 - abs(delta) / t);
-    double py = 1000 / (b * m + l + 1000.0);
-    double pz = 1000 / (max_c + 1000.0);
-    int penalty = (delta < 0 ? - 10000 : 0);
-    return (px + 0.1 * px1) * py * pz + penalty;
+double evaluate_score_px(int m, vector<term_t> const & g, int delta) {
+    constexpr int e = 10000;
+    constexpr int t = 100;
+    if (delta < 0) return - INFINITY;
+    return e * (1 - min<double>(t, delta) / t);
+}
+double evaluate_score_py(int m, vector<term_t> const & g) {
+    constexpr int b = 5;
+    int l = g.size();
+    return 1000 / (b * m + l + 1000.0);
+}
+double evaluate_score_pz(int m, vector<term_t> const & g) {
+    int maxcoeff = get_maxcoeff(g);
+    return 1000 / (maxcoeff + 1000.0);
+}
+double evaluate_score(int m, vector<term_t> const & g, int delta) {
+    constexpr int a = 10000;
+    double px = evaluate_score_px(m, g, delta);
+    double py = evaluate_score_py(m, g);
+    double pz = evaluate_score_pz(m, g);
+    return a * px * py * pz;
 }
 
 template <class Generator>
-double evaluate_relaxed_score(vector<term_t> const & f, int m, vector<term_t> const & g, Generator & gen) {
-    int n = g.size() - m;
-    int l = g.size();
-    int max_c = get_max_c(g);
-    double pa = evaluate_relaxed_score1(m, l, max_c, apply_all_true(g) - apply_all_true(f));
+double evaluate_all_true_score(int n, vector<term_t> const & f, int m, vector<term_t> const & g, Generator & gen) {
+    int delta = apply_all_true_min_sa(n, m, g, gen) - apply_all_true(f);
+    double pa = evaluate_score(m, g, delta);
+    return pa;
+}
+
+template <class Generator>
+double evaluate_random_score(int n, vector<term_t> const & f, int m, vector<term_t> const & g, Generator & gen) {
     double pb = 0;
-    constexpr int width = 300;
-    xor_shift_128 fixed;
+    constexpr int width = 10;
     REP (iteration, width) {
-        constexpr int margin = 0;
-        auto x = generate_random_vector(n, iteration < width ? fixed : gen);
-        int delta = apply_vector_min(g, x, m) - apply_vector(f, x) - margin;
-        pb += evaluate_relaxed_score1(m, l, max_c, delta) / width;
+        auto x = generate_random_vector(n, gen);
+        int delta = apply_vector_min_sa(m, g, x, gen) - apply_vector(f, x);
+        pb += evaluate_score(m, g, delta) / width;
     }
-    return (pa + pb) / 2;
+    return pb;
 }
 
 chrono::high_resolution_clock::time_point clock_begin;
@@ -322,10 +279,9 @@ pair<int, vector<term_t> > solve(int n, int k, vector<term_t> f, Generator & gen
         sort(ALL(t.v));
     }
 
-    // body
+    // prepare
     int m = 0;
     vector<term_t> g;
-
     auto use0 = [&](int c) {
         g.push_back(make_term(c, {}));
     };
@@ -336,40 +292,24 @@ pair<int, vector<term_t> > solve(int n, int k, vector<term_t> f, Generator & gen
         g.push_back(make_term(c, { y1, y2 }));
     };
 
-    auto define = [&](int x1, int x2, int w1) {
-        constexpr int max_c = 100;
-        constexpr int c = 2 * max_c + 10;
-        constexpr int b = c + max_c + 5;
-        constexpr int a = 2 * c - b;
-        assert (b >= 0);  // when x1 = x2 = 0
-        assert (b >= c);  // when x1 = 1 and x2 = 0, or when x1 = 0 and x2 = 1
-        assert (b <= 2 * c and a + b - 2 * c == 0);  // when x1 = x2 = 1
-        assert (b - c - max_c >= 0);  // when - k w1 is used at an other place
-        assert (b - 2 * c + max_c <= 0);  // when + k w1 is used at an other place
-        use2(a, x1, x2);
-        use1(b, w1);
-        use2(- c, x1, w1);
-        use2(- c, x2, w1);
-    };
-    auto squash = [&](int x1, int x2) {
-        int w1 = n + (m ++);
-        define(x1, x2, w1);
-        // cerr << "[*] use " << w1 + 1 << " as " << x1 + 1 << " " << x2 + 1 << endl;
-        return w1;
-    };
-
+    // construct
     for (auto const & t : f) {
+
         if (t.v.size() <= 2) {
+            // the trivial case
             g.push_back(t);
+
         } else if (t.c < 0) {
+            // the simple quadratization of negative monomials
             int d = t.v.size();
             int w1 = n + (m ++);
             use1(- t.c * (d - 1), w1);
             for (int x1 : t.v) {
                 use2(t.c, w1, x1);
             }
-            // cerr << "[*] use " << w1 + 1 << " for the t with c = " << t.c << endl;
+
         } else {
+            // the simple quadratization of positive monomials
             int d = t.v.size();
             auto v = t.v;
             shuffle(ALL(v), gen);
@@ -400,10 +340,13 @@ pair<int, vector<term_t> > solve(int n, int k, vector<term_t> f, Generator & gen
     cerr << "[*] M = " << m << endl;
     cerr << "[*] L = " << g.size() << endl;
     cerr << "[*] f(1) = " << apply_all_true(f) << endl;
-    if (m <= 100) {
+    if (m < 100) {
         cerr << "[*] g(1) = " << apply_all_true_min_sa(n, m, g, gen) << endl;
+        cerr << "[*] score random = " << (int)evaluate_random_score(n, f, m, g, gen) << endl;
+        cerr << "[*] score allone = " << (int)evaluate_all_true_score(n, f, m, g, gen) << endl;
     }
-    // cerr << "[*] score = " << highscore << endl;
+    cerr << "[*] score PY = " << evaluate_score_py(m, g) << endl;
+    cerr << "[*] score PZ = " << evaluate_score_pz(m, g) << endl;
     return make_pair(m, g);
 }
 
